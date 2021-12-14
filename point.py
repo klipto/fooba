e614ccf8fe628e16b04796d4e9c7488424f1eb7b
Todd Mytkowicz <toddm@microsoft.com>
2021-12-14 13:34:18 -0800

    Update point.py

--- point.py
+++ point.py
@@ -1,7 +1,13 @@
+class T:
+    def __init__(self, x):
+        self.x = x
+    def baz(self, x):
+        import os
+        os.getenv("HOME", None)
+        return x + self.x
+    
 def foo(x):
     return bar(x) + 1
 
 def bar(x):
-    import os
-    os.getenv("HOME", None)
-    return x
+    return T(0).baz(x)
